2c2480c956b1fe17edd23c1cc3154c36506954b2
Rafał Kuć <r.kuc@solr.pl>
2020-04-16 21:01:02 +0200

    Modify code

--- src/main/java/com/sematext/blog/solr/ExampleRequestHandler.java
+++ src/main/java/com/sematext/blog/solr/ExampleRequestHandler.java
@@ -1,6 +1,7 @@
 package com.sematext.blog.solr;
 
 import org.apache.solr.common.util.NamedList;
+import org.apache.solr.core.PluginInfo;
 import org.apache.solr.handler.component.SearchHandler;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.SolrQueryResponse;
@@ -10,8 +11,17 @@ import org.apache.solr.response.SolrQueryResponse;
  */
 public class ExampleRequestHandler extends SearchHandler {
   @Override
+  public void init(NamedList args) {
+    super.init(args);
+  }
+
+  @Override
+  public void init(PluginInfo info) {
+    super.init(info);
+  }
+
+  @Override
   public void handleRequestBody(SolrQueryRequest req, SolrQueryResponse rsp) throws Exception {
-    rsp.add("sematext_example", true);
     super.handleRequestBody(req, rsp);
   }
 }
